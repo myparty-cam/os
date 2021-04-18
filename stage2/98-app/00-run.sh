a5cdab6006a408b820e869e035a1d4be483c3324
Silas Rosenkranz <silas@rosenkra.nz>
2021-04-18 19:37:40 +0200

    add: basic electron app

---
+++ stage2/98-app/00-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+install -m 777 files/run-app.sh "${ROOTFS_DIR}/home/mpc/run-app.sh"
+install -m 644 files/mpc-app.service "${ROOTFS_DIR}/etc/systemd/system/mpc-app.service"
+
+on_chroot << EOF
+  wget -O /home/mpc/mpc-1.0.0.arm64.AppImage https://cloud.rosenkra.nz/s/mWscpyT4np6jtSi/download
+
+	chmod +x /home/mpc/run-app.sh
+	chmod +x /home/mpc/mpc-1.0.0.arm64.AppImage
+
+	systemctl enable mpc-app.service
+EOF
---
+++ stage2/98-app/files/run-app.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+xset -dpms
+xset s off
+xset s noblank
+
+unclutter & /home/mpc/mpc-1.0.0.arm64.AppImage
