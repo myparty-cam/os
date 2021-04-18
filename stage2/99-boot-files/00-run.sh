874ed5bbcdb7407751c100494e511aa6e3d124b6
Silas Rosenkranz <silas@rosenkra.nz>
2021-04-18 10:44:33 +0200

    add: splashscreen service

--- stage2/99-boot-files/00-run.sh
+++ stage2/99-boot-files/00-run.sh
@@ -3,6 +3,12 @@
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
 
+install -m 644 files/splash.png "${ROOTFS_DIR}/opt/splash.png"
+install -m 644 files/splashscreen.service "${ROOTFS_DIR}/etc/systemd/system/splashscreen.service"
+
 on_chroot << EOF
 	raspi-config nonint do_boot_wait 1
+
+	systemctl disable getty@tty1
+	systemctl enable splashscreen.service
 EOF
