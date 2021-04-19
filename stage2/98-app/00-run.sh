66b819752ae9bddfc4955f0cdac449c37dfc9f2f
Silas Rosenkranz <silas@rosenkra.nz>
2021-04-19 10:55:50 +0200

    add: tty1 again

--- stage2/98-app/00-run.sh
+++ stage2/98-app/00-run.sh
@@ -6,8 +6,8 @@ install -m 644 files/mpc-app.service "${ROOTFS_DIR}/etc/systemd/system/mpc-app.s
 on_chroot << EOF
   wget -O /home/mpc/mpc-1.0.0.arm64.AppImage https://cloud.rosenkra.nz/s/mWscpyT4np6jtSi/download
 
-	chmod +x /home/mpc/run-app.sh
+	# chmod +x /home/mpc/run-app.sh
 	chmod +x /home/mpc/mpc-1.0.0.arm64.AppImage
 
-	systemctl enable mpc-app.service
+	# systemctl enable mpc-app.service
 EOF
